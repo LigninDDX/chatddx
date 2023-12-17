8158d5e2407e56fd3c8e3d521470a536263f0f4e
Olof Silfver <olof.silfver@gmail.com>
2023-12-17 17:50:06 +0100

    inlogg fixat.

--- gpt/views.py
+++ gpt/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import APIKey, Prompt
 
+from django.contrib.auth.decorators import login_required
+@login_required
 def index(request):
     api_key = APIKey.objects.get(id=1)
     prompts = Prompt.objects.filter(active=True)
