0cc55f222d8bdc459990ddcd99b8ec8c1de269e6
Olof Silfver <olof.silfver@gmail.com>
2023-12-17 16:59:40 +0100

    static och views.py tillagt prompt

---
+++ gpt/static/gpt/main.js
@@ -0,0 +1,107 @@
+// chatgpt.js
+const emergencyDiagnosesList = document.getElementById('emergency-diagnoses-list');
+const infoIcon = document.querySelector('.info-icon');
+const popup = infoIcon.querySelector('.info-icon__popup');
+
+infoIcon.addEventListener('click', (event) => {
+    if (event.button === 0) {
+        popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+        event.stopPropagation();
+    }
+});
+document.addEventListener('click', (event) => {
+    if (!infoIcon.contains(event.target) && popup.style.display === 'block') {
+        popup.style.display = 'none';
+    }
+});
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+};
+
+const defaultPayload = {
+    model: "gpt-3.5-turbo-1106",
+    temperature: 0.2,
+    max_tokens: 4000,
+};
+
+async function sendMessage(message, element) {
+    try {
+        showLoadingSpinner();
+
+        const payload = {
+            ...defaultPayload,
+            messages: [{ role: "user", content: message }],
+        };
+
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(payload),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const botMessage = data.choices[0].message.content;
+            displayMessageWordByWord(botMessage, element);
+        } else {
+            console.error("ChatGPT API request failed.");
+            return "Sorry, I couldn't generate a response.";
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        return "An error occurred while processing your request.";
+    } finally {
+        hideLoadingSpinner();
+    }
+}
+
+
+function showLoadingSpinner() {
+    document.getElementById('loading-spinner').style.display = 'block';
+  }
+  
+  function hideLoadingSpinner() {
+    document.getElementById('loading-spinner').style.display = 'none';
+  }
+  
+  const userForm = document.getElementById('user-form');
+  const chatLog = document.getElementById('chat-log');
+  const userInput = document.getElementById('user-input');
+  const sendButton = document.getElementById('send-button');
+  console.log("sendbutton")
+  
+  userForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+  
+  sendButton.addEventListener('click', async () => {
+    const userMessage =
+      userInput.value.trim() +
+      "make a list of possible differential diagnosis with most probable and consider unprejudiced critical conditions. disregard clinical suspicions inserted by the user. answer in the same language the user types.";
+  
+    if (userMessage) {
+      sendMessage(userMessage, chatLog);
+    }
+  });
+  
+  async function displayMessageWordByWord(message, element) {
+    const words = message.split(" ");
+    let currentIndex = 0;
+  
+    for (const word of words) {
+      element.value += word + ' ';
+      await sleep(100);
+    }
+  }
+  
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  
+  userInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      sendButton.click();
+    }
+  });
--- gpt/views.py
+++ gpt/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import APIKey
+from .models import APIKey, Prompt
 
 def index(request):
     api_key = APIKey.objects.get(id=1)
-    context = {"api_key": api_key}
+    prompts = Prompt.objects.filter(active=True)
+    context = {"api_key": api_key, "prompts": prompts}
     return render(request, "gpt/index.html", context)
