a2c6dd7cb3bc5a32b11739c02524b20828cfb3fd
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-01-09 16:23:06 +0100

    add user to settings

--- chatddx/settings.py
+++ chatddx/settings.py
@@ -22,6 +22,7 @@ except ImportError:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+USER = getenv('user')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
