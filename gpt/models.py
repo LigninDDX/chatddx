f1314e776f066e7d07dd52ab7a178ddac423ad7f
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-01-25 14:13:28 +0100

    open ai api model

--- gpt/models.py
+++ gpt/models.py
@@ -1,5 +1,116 @@
 from django.contrib import admin
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class OpenAIMessageRole(models.Model):
+    class Meta:
+        verbose_name_plural = "OpenAI Message Roles"
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class OpenAIModel(models.Model):
+    class Meta:
+        verbose_name_plural = "OpenAI Models"
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class OpenAILogitBias(models.Model):
+    class Meta:
+        verbose_name_plural = "OpenAI Logit Biases"
+
+    token = models.CharField(max_length=255)
+    bias = models.IntegerField(
+            validators=[
+                MaxValueValidator(100),
+                MinValueValidator(-100),
+                ])
+
+class OpenAIMessage(models.Model):
+    class Meta:
+        verbose_name_plural = "OpenAI Messages"
+
+    content = models.TextField()
+    role = models.ForeignKey(OpenAIMessageRole, on_delete=models.PROTECT)
+    name = models.CharField(
+            max_length=255,
+            default=None,
+            null=True,
+            blank=True,
+            )
+
+    def __str__(self):
+        return f"{self.role.name}: {self.content[:10]}..."
+
+class OpenAIChat(models.Model):
+    class Meta:
+        verbose_name_plural = "OpenAI Chat Configuration"
+
+    def __str__(self):
+        return self.endpoint
+
+    active = models.BooleanField(default=True)
+    endpoint = models.CharField(max_length=255)
+    api_key = models.CharField(max_length=255)
+    messages = models.ManyToManyField(OpenAIMessage)
+    model = models.ForeignKey(OpenAIModel, on_delete=models.PROTECT)
+
+    frequency_penalty = models.FloatField(
+            default=0,
+            validators=[
+                MinValueValidator(-2),
+                MaxValueValidator(2),
+                ])
+
+    logit_bias = models.ForeignKey(
+            OpenAILogitBias,
+            on_delete=models.PROTECT,
+            null=True,
+            default=None,
+            blank=True,
+            )
+
+    max_tokens = models.IntegerField(
+            validators=[
+                MinValueValidator(1),
+                MaxValueValidator(2000),
+                ])
+
+    presence_penalty = models.FloatField(
+            default=0,
+            validators=[
+                MinValueValidator(-2),
+                MaxValueValidator(2),
+                ])
+
+    temperature = models.FloatField(
+            default=1,
+            validators=[
+                MinValueValidator(0),
+                MaxValueValidator(2),
+                ]
+            )
+
+    top_p = models.FloatField(
+            default=1,
+            validators=[
+                MinValueValidator(0),
+                MaxValueValidator(1),
+                ]
+            )
+
+    user = models.CharField(
+            max_length=255,
+            default=None,
+            blank=True,
+            null=True,
+            )
 
 class APIKey(models.Model):
     key = models.CharField(max_length=255)
@@ -19,3 +130,9 @@ class Prompt(models.Model):
 admin.site.register(Prompt)
 admin.site.register(APIKey)
 admin.site.register(ChatGPTResult)
+
+admin.site.register(OpenAILogitBias)
+admin.site.register(OpenAIChat)
+admin.site.register(OpenAIModel)
+admin.site.register(OpenAIMessage)
+admin.site.register(OpenAIMessageRole)
