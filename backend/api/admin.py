63fb551a5e0797f45ac6f135ef29e4d1482de63b
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-05-07 18:13:46 +0200

    inlcude more details in prompt history

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from . import models
 
+class PromptHistoryAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'config', 'user', 'prompt', 'response']
+
 admin.site.register(models.OpenAIChatCluster)
 admin.site.register(models.OpenAIChat)
 admin.site.register(models.OpenAIMessage)
 admin.site.register(models.OpenAIMessageRole)
 admin.site.register(models.OpenAIModel)
 admin.site.register(models.OpenAILogitBias)
-admin.site.register(models.PromptHistory)
+admin.site.register(models.PromptHistory, PromptHistoryAdmin)
