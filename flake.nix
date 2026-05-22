33bb909dd8b903434eb2a651f736fae90ec29576
alex <alex@kompismoln.se>
2026-05-22 03:46:24 +0200

    deploy #3

--- flake.nix
+++ flake.nix
@@ -57,61 +57,27 @@
             ]
           )
       );
-    in
-    {
-      inherit workspace pythonSets;
+      django-app =
+        system: (pythonSets.${system}.mkVirtualEnv "${name}-django-${version}" workspace.deps.default);
 
-      packages = forAllSystems (
+      scripts =
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
-        rec {
-          svelte-app = pkgs.stdenv.mkDerivation {
-            pname = "${name}-client";
-            inherit version;
-            src = ./client;
+        pkgs.runCommand "chatddx-commands" { } ''
+          mkdir -p $out/bin
+          ln -s ${django-app system}/bin/chatddx $out/bin/chatddx
+          ln -s ${django-app system}/bin/django $out/bin/chatddx-django
+        '';
+    in
+    {
+      inherit workspace pythonSets;
 
-            nativeBuildInputs = with pkgs; [
-              nodejs
-              pnpm
-              pnpmConfigHook
-              makeWrapper
-            ];
-
-            pnpmDeps = pkgs.fetchPnpmDeps {
-              pname = name;
-              src = ./client;
-              fetcherVersion = 2;
-              hash = "sha256-gQPPm/ymE1nuXQdFqRtQIgR7ON3EFV0c7De+F34dRKc=";
-            };
-
-            buildPhase = ''
-              runHook preBuild
-              pnpm build
-              runHook postBuild
-            '';
-
-            installPhase = ''
-              mkdir -p $out/{lib,bin}
-              cp -r build $out/lib/
-              makeWrapper ${pkgs.nodejs}/bin/node $out/bin/run \
-                --add-flags "$out/lib/build/index.js" \
-                --set NODE_ENV production
-            '';
-          };
-
-          # workspace.deps.default excludes dev/test dependency-groups
-          django-app = pythonSets.${system}.mkVirtualEnv "${name}-django-${version}" workspace.deps.default;
-
-          django-manage = pkgs.writeShellApplication {
-            name = "${name}-django-manage-${version}";
-            # manage is also a stand-alone script
-            text = builtins.readFile ./backend/src/${name}/django/bin/manage;
-          };
-
-        }
-      );
+      packages = forAllSystems (system: {
+        django-app = django-app system;
+        scripts = scripts system;
+      });
 
       devShells = forAllSystems (
         system:
